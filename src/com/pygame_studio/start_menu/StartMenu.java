02f9def35169fb6d7c08db555d13a461e0d42ac6
Eno-Gerguri <enogerguri@outlook.com>
2020-05-25 20:55:41 +0100

    Initialised the StartMenu

---
+++ src/com/pygame_studio/start_menu/StartMenu.java
@@ -0,0 +1,14 @@
+/**
+ * Controls the frame that displays the start menu and then displays the start menu
+ */
+package com.pygame_studio.start_menu;
+
+public class StartMenu {
+	/**
+	 * Controls the frame that displays the start menu and then displays the start menu
+	 */
+	public StartMenu() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/pygame_studio/start_menu/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ * Handles and Deals with all aspects to do with the Start Menu in Pygame Studio
+ * 
+ */
+
+package com.pygame_studio.start_menu;
